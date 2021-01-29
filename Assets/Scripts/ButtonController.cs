5beaf87d0a3e9152cfd76312afca8173c3ed05d6
slayer-tej <aruntejagurrapu@gmail.com>
2021-01-29 10:47:53 +0530

    Added Ui for Game with interactable buttons and text

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField]
+    private string Playerside;
+    [SerializeField]
+    private Button button;
+    [SerializeField]
+    private TextMeshProUGUI buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = Playerside;
+        button.interactable = false;
+
+    }
+
+}
